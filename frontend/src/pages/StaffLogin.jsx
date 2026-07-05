6a3a5a4078c5d11924c6887e747a5baf84954696
Nagaraj Vatagal <nagarajvatagal87@gmail.com>
2026-07-05 15:15:49 +0530

    Fix nested Git repositories

--- frontend/src/pages/StaffLogin.jsx
+++ frontend/src/pages/StaffLogin.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Stafflogin.css";
+import "./stafflogin.css";
 
 export default function StaffLogin() {
   const navigate = useNavigate();
